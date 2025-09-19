445119f893e67139d5856e26add182977b999490
Luna <20017802+lunaboppin@users.noreply.github.com>
2025-09-20 00:32:59 +0100

    Revert "Show pin creation date in dashboard and pins list"
    This reverts commit 6961730b9166227aed53dab70ee0d01929741d55.

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
+
+// Always accessible: pins JSON endpoint
+Route::get('/pins/json', [\App\Http\Controllers\PinController::class, 'json'])->name('pins.json');
+
 Route::middleware('guest')->group(function () {
     Route::get('/login', function () {
         return Socialite::driver('authentik')->redirect();
     })->name('login');
-    Route::get('/pins/json', [\App\Http\Controllers\PinController::class, 'json'])->name('pins.json');
-    
     Route::get('/auth/callback', [AuthController::class, 'callback']);
 });
 
@@ -34,7 +36,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/pins/{pin}/edit', [\App\Http\Controllers\PinController::class, 'edit'])->name('pins.edit');
     Route::put('/pins/{pin}', [\App\Http\Controllers\PinController::class, 'update'])->name('pins.update');
     Route::delete('/pins/{pin}', [\App\Http\Controllers\PinController::class, 'destroy'])->name('pins.destroy');
-    Route::get('/pins/json', [\App\Http\Controllers\PinController::class, 'json'])->name('pins.json');
     Route::get('/pins/create', [\App\Http\Controllers\PinController::class, 'create'])->name('pins.create');
     Route::post('/pins', [\App\Http\Controllers\PinController::class, 'store'])->name('pins.store');
 });
